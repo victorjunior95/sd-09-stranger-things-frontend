bd72d82167b9afc727c7c8ee787892137fcc3443
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-08-13 22:30:39 -0300

    Fix requisito 09

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -115,9 +115,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-        {process.env.REACT_APP_DEVELOPMENT
-          ? <p>Em desenvolvimento</p>
-          : '' }
+        {process.env.REACT_APP_DEVELOPMENT && <p>Em desenvolvimento</p>}
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
