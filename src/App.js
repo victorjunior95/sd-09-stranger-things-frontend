d50e5373dc54ba426c2c5206221931eda36dc83b
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-08-14 22:55:49 -0300

    correção env

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      { process.env.DEV_MODE === 'true' && <h2>Em desenvolvimento</h2> }
+      { process.env.REACT_APP_DEV_MODE === 'true' && <h2>Em desenvolvimento</h2> }
       <StrangerThings />
     </div>
   );
