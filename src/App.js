15bedd2d418c5a1fa10d27651c050dc25e5e531c
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-08-14 22:32:34 -0300

    dev mode

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      { process.env.DEV_MODE === 'true' && <h2>Em desenvolvimento</h2> }
       <StrangerThings />
     </div>
   );
