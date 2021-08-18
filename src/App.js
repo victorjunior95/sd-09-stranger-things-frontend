a44107443cc9eb9494f594bec6bf365574390be9
antonioarieiro <sfgfffs@gmail.com>
2021-08-18 09:18:04 -0300

    mult amb

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+import TagComponent from './components/TagComponent';
 
-/* PROJETO FINALIZADO EM PARCERIA COM A TRICA */
+require('dotenv').config();
+
+const devAmb = process.env.REACT_APP_TAG_DEV === 'true';
 
 function App() {
   return (
     <div className="App">
+      { devAmb && <TagComponent /> }
       <StrangerThings />
     </div>
   );
---
+++ src/components/TagComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dev = () => (
+  <div>
+    <h1>Em desenvolvimento</h1>
+  </div>
+);
+
+export default Dev;
