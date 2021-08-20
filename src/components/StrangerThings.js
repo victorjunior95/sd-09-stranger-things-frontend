2264db35a1a359244dac65c6b020b18691674183
Victor <moraesvictorgk@gmail.com>
2021-08-20 01:06:15 -0300

    first commit and create all structure

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,17 +7,18 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
 const charactersUpsideDownService = new CharactersService(upsideDownConfig);
+const inDev = (process.env.REACT_APP_DEVELOPMENT === 'true');
 
 class StrangerThings extends React.Component {
   constructor(props) {
@@ -120,7 +121,7 @@ class StrangerThings extends React.Component {
               Mudar de Realidade
             </button>
           </div>
-
+          { inDev && <button type="submit">Em desenvolvimento</button> }
           <div>
             <input
               placeholder="Nome do Personagem"
